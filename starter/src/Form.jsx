f627b20762168f1f07f7303e30ed31604c1e29f0
Gabor Szabo1 <gabor_szabo1@epam.com>
2024-01-31 13:43:44 +0100

    REACT QUERY

---
+++ starter/src/Form.jsx
@@ -0,0 +1,36 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
+import customFetch from "./utils";
+import { useCreateTask } from "./reactQueryCustomHooks";
+
+const Form = () => {
+  const [newItemName, setNewItemName] = useState("");
+
+  const { createTask, isLoading } = useCreateTask();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createTask(newItemName, {
+      onSuccess: () => {
+        setNewItemName("");
+      },
+    });
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <h4>task bud</h4>
+      <div className="form-control">
+        <input
+          type="text "
+          className="form-input"
+          value={newItemName}
+          onChange={(event) => setNewItemName(event.target.value)}
+        />
+        <button type="submit" className="btn" disabled={isLoading}>
+          add task
+        </button>
+      </div>
+    </form>
+  );
+};
+export default Form;
